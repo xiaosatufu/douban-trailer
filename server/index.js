9dfc755c9f436cbc69c6821ae33f83c1e0a83f7e
yongshuang <yongshuang@gdgyun.com>
2019-03-20 16:06:27 +0800

    合并分支

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const Koa = require('koa')
 const app = new Koa()
-const {normal}  = require('./tpl')
+const {htmlTpl,ejsTpl,pugTpl}  = require('./tpl')
+const ejs  = require('ejs')
+const pug = require('pug')
 app.use(async (ctx,next)=>{
     ctx.type='text/html;charset=utf-8'
-    ctx.body = normal
+    // ctx.body = htmlTpl
+    ctx.body = pug.render(pugTpl,{
+        you:'222',
+        me:'ddd'
+    })
 })
 
 app.listen('2333')
---
+++ server/tpl/ejs.js
@@ -0,0 +1,29 @@
+module.exports = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <title>Document</title>
+        <link rel="stylesheet" href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css"/>
+        <script src="https://cdn.bootcss.com/jquery/3.3.0/jquery.min.js"></script>
+        <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1><%=you%></h1>
+                    <p><%=me%></p>
+                </div>
+                <div class="col-md-4">
+                    <p>
+                        test
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+`
