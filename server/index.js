41063ef77606f182dd17a1aa09284574753c673f
yongshuang <yongshuang@gdgyun.com>
2019-03-20 16:23:29 +0800

    增加模板引擎中间键

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const Koa = require('koa')
 const app = new Koa()
-const {htmlTpl,ejsTpl,pugTpl}  = require('./tpl')
-const ejs  = require('ejs')
-const pug = require('pug')
+const views = require('koa-views')
+const {resolve} = require('path')
+
+app.use(views(resolve(__dirname,'./views'),{
+    extension:'pug'
+}))
 app.use(async (ctx,next)=>{
-    ctx.type='text/html;charset=utf-8'
-    // ctx.body = htmlTpl
-    ctx.body = pug.render(pugTpl,{
-        you:'222',
-        me:'ddd'
+    await ctx.render('index',{
+        you:'dddd',
+        me:'2ddd'
     })
 })
 
